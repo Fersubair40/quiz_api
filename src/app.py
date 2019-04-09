3d9b5c1fb667920fd7ee4fa30610ced30ae608e0
Subair Temilayo <olajnrsubair@gmail.com>
2019-04-09 01:14:42 +0100

    created first endpoint

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import  Flask
+
+from src.config import app_config
+
+def create_app(env_name):
+
+
+    app = Flask(__name__)
+
+    app.config.from_object(app_config[env_name])
+
+    @app.route('/', methods=['GET'])
+
+    def index():
+         
+        #  testing endpoint
+
+        return 'endpoint is workin'
+    
+    return app
+
+
---
+++ src/config.py
@@ -0,0 +1,28 @@
+import os
+
+
+class Development(object):
+    
+    # Development environment configuration
+
+    DEBUG = True
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+
+class Production(object):
+
+    # Production environment configuration
+
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+
+
+app_config = {
+    'development': Development,
+    'production': Production,
+}
+
+
